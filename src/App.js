2310b8f04f63de14524bdf7934a19ded9cb27150
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-01-12 11:23:30 +0100

    Adding files for github

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -20,6 +22,10 @@ class App extends Component {
       .then((response) => response.json())
       .then((user) => this.setState({ monsters: user }));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
@@ -27,9 +33,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="Search Monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
